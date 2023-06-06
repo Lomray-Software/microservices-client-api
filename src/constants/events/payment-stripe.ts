43dd2adc6b31ee1bba592ae7ccda8faf8921b052
Oleg Drozdovich <44732463+OlegDO@users.noreply.github.com>
2023-06-06 20:13:36 +0400

    feat: add cards events (#15)

--- src/constants/events/payment-stripe.ts
+++ src/constants/events/payment-stripe.ts
@@ -3,6 +3,9 @@
  */
 enum Event {
   EntityPaid = 'event.payment-stripe.provider.entity-paid',
+  SetupIntentSucceeded = 'event.payment-stripe.provider.setup-intent-succeeded',
+  PaymentMethodUpdated = 'event.payment-stripe.provider.payment-method-updated',
+  PaymentMethodRemoved = 'event.payment-stripe.provider.payment-method-removed',
 }
 
 export default Event;
