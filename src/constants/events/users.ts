06a7b8e3980db5d6a1fec4f23ce442328006f014
Oleg Drozdovich <drozdovich.oleg1999@gmail.com>
2023-06-26 13:15:28 +0400

    feat(users): add users event on create and update

--- src/constants/events/users.ts
+++ src/constants/events/users.ts
@@ -2,6 +2,8 @@
  * Users events
  */
 enum Event {
+  UserCreate = 'event.users.user.create',
+  UserUpdate = 'event.users.user.update',
   UserRemove = 'event.users.user.remove',
   UserRestore = 'event.users.user.restore',
 }
