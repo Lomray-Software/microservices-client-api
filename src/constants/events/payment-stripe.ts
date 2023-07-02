24af84bd0e074ffe0bd154db16b9c66f9ca3aebc
Oleg Drozdovich <drozdovich.oleg1999@gmail.com>
2023-07-03 03:04:15 +0400

    feat(payment-stripe): add payment intent events

--- src/constants/events/payment-stripe.ts
+++ src/constants/events/payment-stripe.ts
@@ -6,6 +6,9 @@ enum Event {
   SetupIntentSucceeded = 'event.payment-stripe.provider.setup-intent-succeeded',
   PaymentMethodUpdated = 'event.payment-stripe.provider.payment-method-updated',
   PaymentMethodRemoved = 'event.payment-stripe.provider.payment-method-removed',
+  PaymentIntentError = 'event.payment-stripe.provider.payment-intent-error',
+  PaymentIntentInProcess = 'event.payment-stripe.provider.payment-intent-in-process',
+  PaymentIntentSuccess = 'event.payment-stripe.provider.payment-intent-success',
 }
 
 export default Event;
