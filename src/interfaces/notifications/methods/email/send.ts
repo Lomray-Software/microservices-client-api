ca80b5920722d8ed03223b7397f47114b4f7b04f
Oleg Drozdovich <drozdovich.oleg1999@gmail.com>
2023-07-06 00:25:01 +0400

    feat(notification): update email input

--- src/interfaces/notifications/methods/email/send.ts
+++ src/interfaces/notifications/methods/email/send.ts
@@ -1,9 +1,19 @@
+export interface IAttachment {
+  filename?: string | false | undefined;
+  // Base64, hex or a binary
+  encoding?: string | undefined;
+  // String, Buffer or a Stream contents for the attachmentent
+  content?: string;
+}
+
 export interface IEmailSendInput {
-  from?: string; // it can be default value
   to: string[];
   subject: string;
   text: string;
   html: string;
+  // It can be default value
+  from?: string;
+  attachments?: IAttachment[];
 }
 
 export interface IEmailSendOutput {
