442cf48bfadd4f5b099a94ca5aafb7cc3a96bc0d
Oleg Drozdovich <drozdovich.oleg1999@gmail.com>
2023-11-23 20:03:30 +0400

    feat(payment-stripe): update transaction entity tax refs

--- src/interfaces/payment-stripe/entities/transaction.ts
+++ src/interfaces/payment-stripe/entities/transaction.ts
@@ -9,7 +9,6 @@ import type ICustomer from './customer';
 import type IProduct from './product';
 
 export interface IComputedTax {
-  taxId?: ITax['id'];
   taxTransactionAmountWithTaxUnit?: ITax['transactionAmountWithTaxUnit'];
   taxExpiresAt?: ITax['expiresAt'];
   taxCreatedAt?: ITax['createdAt'];
@@ -17,18 +16,39 @@ export interface IComputedTax {
   taxBehaviour?: ITax['behaviour'];
   totalTaxPercent?: ITax['totalTaxPercent'];
   taxFeeUnit?: number;
+  taxAutoCalculationFeeUnit?: number;
 }
 
+/**
+ * Transaction params
+ * @description Definitions:
+ * 1. TransferId - Stripe object is created when you move funds between Stripe accounts as part of Connect. Needed for comprehending
+ * the transaction state in situations involving the reversal of the full or partial transaction amount and refund with reversal.
+ * For instance: py_1OFKUmPBMR5FbqzbQT2N5juH
+ * 2. DestinationTransactionId - Stripe destination transaction id. Reference regarding the destination transaction on the connected account's side
+ * For instance:  tr_3OFKatAmQ4asS8PS0GlS9dUr
+ * 3. PersonalFee - Personal user fee. For receiver, it's application fees with only debit extra fees.
+ *  For sender, it's application fees with only credit extra fees.
+ */
 export interface IParams extends IComputedTax {
+  // Refunded original transaction/payment intent/charge
   refundedTransactionAmount: number;
+  // Refunded Stripe collected fee
   refundedApplicationFeeAmount: number;
-  // Decomposed fees
+  // Transferred amount (e.g. via destination payment intent)
+  transferAmount: number;
+  // Reversed transaction amount from DESTINATION (e.g. connect account) to source (e.g. Platform)
+  transferReversedAmount: number;
+  // Platform account fee
   platformFee: number;
+  // Stripe fee for processing transaction
   stripeFee: number;
   baseFee: number;
   extraFee: number;
-  // Personal user fee. Receiver it's application fees with only debit extra fees. Sender it's application fees with only credit extra fees.
   personalFee: number;
+  destinationTransactionId?: string | null;
+  transferId?: string | null;
+  transferDestinationConnectAccountId?: string;
   paymentStatus?: StripeTransactionStatus;
   checkoutStatus?: StripeCheckoutStatus;
   errorMessage?: string;
@@ -36,10 +56,8 @@ export interface IParams extends IComputedTax {
   errorCode?: string;
   // Example: generic_decline
   declineCode?: string;
-  // Application and stripe fees payer
+  // Stripe and platform fee payer
   feesPayer?: TransactionRole;
-  // PaymentIntent charge id, must exist for refund
-  chargeId?: string;
   extraRevenue?: number;
   // Amount that will charge for instant payout
   estimatedInstantPayoutFee?: number;
@@ -53,6 +71,12 @@ export interface IParams extends IComputedTax {
 interface ITransaction extends IEntity {
   id: string;
   transactionId?: string;
+  // Stripe Tax Calculation API - calculated by Stripe tax for transaction (e.g. payment intent
+  taxCalculationId?: string | null;
+  // Stripe Tax Transaction API - Stripe tax transaction presented in Stripe Tax reports
+  taxTransactionId?: string | null;
+  // Represents a single attempt to move money into Platform Stripe account. Required for refunds, reversals, fees
+  chargeId?: string | null;
   applicationFeeId?: string | null;
   title?: string;
   userId?: string;
