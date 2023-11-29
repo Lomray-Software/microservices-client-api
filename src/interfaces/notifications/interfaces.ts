1d430b5cdf880b772890bcf7b2c30f3a3c562b21
Oleg Drozdovich <drozdovich.oleg1999@gmail.com>
2023-11-29 13:23:12 +0400

    feat(notification): add task job endpoint

---
+++ src/interfaces/notifications/interfaces.ts
@@ -0,0 +1,9 @@
+interface IHandledCounts {
+  // Total tasks processed
+  total: number;
+  completed: number;
+  failed: number;
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export { IHandledCounts };
---
+++ src/interfaces/notifications/jobs/task/process.ts
@@ -0,0 +1,8 @@
+import type { IHandledCounts } from '../../interfaces';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface ITaskProcessInput {}
+
+interface ITaskProcessOutput extends IHandledCounts {}
+
+export { ITaskProcessInput, ITaskProcessOutput };
