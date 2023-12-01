c65feb0a29b09a6c7ef0c8d3fccc04a50bdbefe4
Oleg Drozdovich <44732463+OlegDO@users.noreply.github.com>
2023-12-01 20:43:48 +0400

    feat(authentication): update token entity (#73)

--- src/interfaces/authentication/entities/token.ts
+++ src/interfaces/authentication/entities/token.ts
@@ -1,10 +1,27 @@
 import type { IEntity } from '@lomray/microservices-types';
+import type TokenType from '../../../constants/token-type';
+
+export interface IUserParams {
+  pushNotificationToken?: string;
+  [key: string]: any;
+}
 
 interface IToken extends IEntity {
-  id: string;
-  userParams?: {
-    pushNotificationToken?: string;
-  };
+  id?: string;
+  type?: TokenType;
+  userId?: string;
+  personal?: string | null;
+  access?: string | null;
+  refresh?: string | null;
+  // Maximum token expiration time
+  expirationAt?: number | null;
+  // Some token data, like device type and etc.
+  params?: Record<string, any>;
+  // Some payload data for token.
+  jwtPayload?: Record<string, any>;
+  // Some user payload data included in tokens.
+  userParams?: IUserParams;
+  createdAt?: Date;
 }
 
 export default IToken;
